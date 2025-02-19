d4c99ee975e8b635a04cdbbd8c219f01fbbd067a
sorupujara92 <souravpujara@gmail.com>
2025-02-19 18:04:43 +0530

    vm

---
+++ terraform/com1.tf
@@ -0,0 +1,34 @@
+resource "google_compute_instance" "vm_instance_project_a" {
+  name         = "terraform-instance12"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+     access_config {
+   }
+
+  }
+
+
+  metadata = {
+    enable-oslogin = "true"
+  }
+
+  metadata_startup_script = <<-EOF
+  #!/bin/bash
+  host_ip=$(curl ifconfig.me)
+  awx_url='https://awx.z-apps.io/api/v2/job_templates/22/launch/'
+  awx_token=''
+  data="{\"limit\": \"$host_ip\"}"
+  curl -X POST "$awx_url" -H "Authorization: Bearer $awx_token" -H "Content-Type: application/json" -d "$data"
+  EOF
+  zone="us-central1-a"
+  project="samad-450009"
+}
+
